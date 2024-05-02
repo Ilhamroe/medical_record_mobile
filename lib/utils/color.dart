8305211dbf24ceab45c0d57a2ec9aed517d7c010
iramadani2003@gmail.com <iramadani2003@gmail.com>
2024-05-02 10:19:13 +0700

    first commit

---
+++ lib/utils/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color themeDark = Color(0xFF30ADA2);
+const Color themeLight = Color(0xFF52D1C6);
+
+const Color danger = Color(0xFFFF5C5C);
+const Color navbar = Color(0xFFADADAD);
+const Color textColor = Color(0xFF717784);
+const Color pureWhite = Color(0xFFFFFFFF);
+const Color fieldColor = Color(0xFFF4F4F4);
+const Color today = Color(0xFFFFD749);
+const Color card = Color(0xFFE8F3F1);
